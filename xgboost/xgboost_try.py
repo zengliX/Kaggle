fdc9bf923dd6bbbf4b99ae040a7d604784ed95c5
Li Zeng <li.zeng.ct@gmail.com>
2017-06-30 14:40:13 -0400

    parameter tuning

--- xgboost/xgboost_try.py
+++ xgboost/xgboost_try.py
@@ -8,14 +8,20 @@ sys.path.append(os.path.realpath(os.curdir)+'/..')
 import xgboost as xgb
 import pandas as pd
 import numpy as np
+import matplotlib
+matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from sklearn.metrics import r2_score
 from sklearn.model_selection import KFold
+import itertools
+import pickle
+import time
 
 # command line inputs
 #input_fd = '../data/cleaned2'
-#output_fd = './xgb_cleaned2_first8'
-_, input_fd, output_fd = sys.argv
+#output_fd = './temp'
+input_fd=sys.argv[1] 
+output_fd = sys.argv[2]
 
 if not os.path.exists(output_fd):
     os.makedirs(output_fd)
@@ -31,11 +37,13 @@ y = TRAIN.y
 del TRAIN['y']
 del TEST['y']
 
+
 X0_cols = list(filter(lambda x: 'X0'+'_' in x, TRAIN.columns))
 TRAIN.drop(X0_cols,axis=1,inplace=True)
 TEST.drop(X0_cols,axis=1,inplace=True)
 
 
+"""
 # only use X1 - X8 + new group
 keep = ['X1','X2','X3','X4','X5','X6','X8','new_group']
 exp_keep = []
@@ -46,28 +54,50 @@ len(exp_keep)
 
 TRAIN = TRAIN.loc[:,exp_keep]
 TEST = TEST.loc[:,exp_keep]
+"""
 
+"""----------------------
+GENERATE PARAMETERS
+----------------------""" 
 
+def param_gen(eta,max_depth,sub,col_sub):
+    out= []
+    for e,m,s,cs in itertools.product(eta,max_depth,sub,col_sub):
+        xgb_params = {
+        'eta': e,
+        'max_depth': m,
+        'subsample': s,
+        'objective': 'reg:linear',
+        'eval_metric': 'rmse',
+        'colsample_bytree':cs,
+        'silent': 1}
+        out.append(xgb_params)
+    return out
+
+#params_list = param_gen(ntree=[500,400,600],eta=[.05,.02,.1],max_depth=[2,3,4,5,6],sub=[1,.8,.9])
+params_list = param_gen(eta=[.05,.01,.005],max_depth=[2,3,4,5],sub=[1,.8,.9],col_sub=[.3,.6,1])
+
+#params_list = param_gen(ntree=[500],eta=[0.1,.05],max_depth=[2,3],sub=[1])
 
 """----------------------
 CROSS VALIDATION IN TRAINING
 ----------------------""" 
+np.random.seed(23)
 
-
-
+"""
 xgb_params = {
-    'n_trees': 500, 
     'eta': 0.05,
-    'max_depth': 4,
-    'subsample': 0.95,
+    'max_depth': 2,
+    'subsample': 0.9,
+    'colsample_bytree':0.7,
     'objective': 'reg:linear',
     'eval_metric': 'rmse',
     'silent': 1
 }
-
+"""
 
 def myCV(xgb_params):
-    numFolds = 5
+    numFolds = 10
     kf = KFold(n_splits= numFolds ,shuffle = True)
     kf.get_n_splits(TRAIN)
 
@@ -84,12 +114,12 @@ def myCV(xgb_params):
         dtest = xgb.DMatrix(X_test,y_test)
         cv_result = xgb.cv(xgb_params, 
                            dtrain, 
-                           num_boost_round=1000, # increase to have better results (~700)
-                           early_stopping_rounds=100,
+                           num_boost_round=3000, # increase to have better results (~700)
+                           early_stopping_rounds=40,
                            verbose_eval=50, 
                            show_stdv=False
                            )
-        niter = np.argmin(cv_result['test-rmse-mean']) # find best iteration
+        niter = cv_result.shape[0]
         model = xgb.train(dict(xgb_params, silent=0), dtrain, num_boost_round=niter)
         out['train_r2'].append(r2_score(dtrain.get_label(), model.predict(dtrain)))
         out['test_r2'].append(r2_score(dtest.get_label(), model.predict(dtest)))
@@ -98,22 +128,47 @@ def myCV(xgb_params):
     out['test_r2_mean']=np.mean(out['test_r2'])
     return out
 
-myCV(xgb_params)
+ct= 0
+cv_out = []
+t0 = time.time()
+for xgb_params in params_list:
+    print('------------------------------------------------------\n')
+    print("working on parameters:",xgb_params,'\n')
+    print("index:",ct,'\n')
+    cur_cv = myCV(xgb_params)
+    cv_out.append({'pars':xgb_params,'fit':cur_cv})
+    ct+=1
+    
+    # report
+    print("current cv results:",cur_cv)
+    speed = (time.time()-t0)/ct
+    print('time remaining:',(len(params_list)-ct)*speed/60,'mins\n')
+    print('------------------------------------------------------')
+    
+# save cv_results
+cv_file = output_fd+'/cv_out.pckl'
+f = open(cv_file,'wb')
+pickle.dump(cv_out,f)
+f.close()
+print('------------------------------------------------------')
+print('mean-test-r2:',[x['fit']['test_r2_mean'] for x in cv_out])
+print('------------------------------------------------------')
+
+
+# final parameter
+# remove highest and lowest
+test_r2 = [x['fit']['test_r2'] for x in cv_out]
+def temp_f(x):
+    return np.sort(x)[1:-1].mean()
+new_mean = list(map(temp_f,test_r2))
+final_params = cv_out[np.argmax(new_mean)]['pars']
+
+#final_params = cv_out[np.argmax([x['fit']['test_r2_mean'] for x in cv_out])]['pars']
+
 
 """----------------------
 FINAL MODEL
 ----------------------""" 
-
-xgb_params = {
-    'n_trees': 500, 
-    'eta': 0.05,
-    'max_depth': 4,
-    'subsample': 0.95,
-    'objective': 'reg:linear',
-    'eval_metric': 'rmse',
-    'silent': 1
-}
-
 
 # form DMatrices for xgb training
 dtrain = xgb.DMatrix(TRAIN, y, feature_names=TRAIN.columns.values)
@@ -121,33 +176,48 @@ dtest = xgb.DMatrix(TEST)
 
 
 # xgb, cross-validation
-cv_result = xgb.cv(xgb_params, 
+cv_result = xgb.cv(final_params, 
                    dtrain, 
-                   num_boost_round=1000, # increase to have better results (~700)
-                   early_stopping_rounds=100,
+                   num_boost_round=3000, # increase to have better results (~700)
+                   early_stopping_rounds=20,
                    verbose_eval=50, 
                    show_stdv=False
                   )
+#temp = cv_result.iloc[-1,:]
+#sd1_val = temp['test-rmse-mean'] + 0.5*temp['test-rmse-std'] # 1 std from mininum
+#niter_1sd = cv_result.index[cv_result['test-rmse-mean']<sd1_val][0]
+niter = cv_result.shape[0]
 
-niter = np.argmin(cv_result['test-rmse-mean']) # find best iteration
-        
 # train model
-model = xgb.train(dict(xgb_params, silent=0), dtrain, num_boost_round=niter)
+# model1 = xgb.train(dict(final_params, silent=0), dtrain, num_boost_round=niter_1sd)
+model2 = xgb.train(dict(final_params, silent=0), dtrain, num_boost_round=niter)
 
 # score
-r2_score(dtrain.get_label(), model.predict(dtrain))
+# r2_train1 = r2_score(dtrain.get_label(), model1.predict(dtrain))
+# print("R2 on training:",r2_train1,'\n')
+
+r2_train2 = r2_score(dtrain.get_label(), model2.predict(dtrain))
+print('------------------------------------------------------')
+print("R2 on training:",r2_train2,'\n')
+print('------------------------------------------------------')
+
 
 """----------------------
 PREDICTION
 ----------------------""" 
 
 # make predictions and save results
-y_pred = model.predict(dtest)
-output = pd.DataFrame({'ID': TEST.index, 'y': y_pred})
-output.to_csv(output_fd+'/XGB_first8.csv')
+# y_pred = model1.predict(dtest)
+# output = pd.DataFrame({'ID': TEST.index, 'y': y_pred})
+# output.to_csv(output_fd+'/XGB_tuned1.csv',index=False)
 
-fig, ax = plt.subplots(figsize=(12,18))
-xgb.plot_importance(model, max_num_features=80, height=0.8, ax=ax)
+y_pred = model2.predict(dtest)
+output = pd.DataFrame({'ID': TEST.index, 'y': y_pred})
+output.to_csv(output_fd+'/XGB_tuned2.csv',index=False)
+
+
+fig, ax = plt.subplots(figsize=(12,30))
+xgb.plot_importance(model2,height=0.8, ax=ax)
 fig.savefig(output_fd+'/imp.pdf')
 
 
